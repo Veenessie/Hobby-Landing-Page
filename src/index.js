22738ce7c60c224bab2e2b85bef2d5a030cda03e
Verity <mastersverity@gmail.com>
2024-02-13 11:12:27 +0000

    Added prompts to learn to ride button in js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ function changeTheme() {
   body.classList.toggle("dark");
 }
 
+function bookLesson() {
+  let name = prompt("What is your name?");
+  let email = prompt("What is your email?");
+
+  name = name.trim();
+
+  alert(`Yay, ${name} your lesson is booked 🐴`);
+}
+
 let themeButton = document.querySelector("#theme-button");
 themeButton.addEventListener("click", changeTheme);
+
+let LearnToRideButton = document.querySelector("#learn-button");
+LearnToRideButton.addEventListener("click", bookLesson);
